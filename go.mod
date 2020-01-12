f7674b6b8aef0036b37f50b1c1d2b78e52d1c70d
jinho hong <tpdleps@gmail.com>
2020-01-12 19:46:56 +0900

    init

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-with-game
 
 go 1.13
+
+require github.com/hajimehoshi/ebiten v1.10.2
--- main.go
+++ main.go
@@ -1,8 +1,29 @@
-package go_with_game
+package main
 
-func main (){
-	for{
-		update()
-		draw()
+import (
+	"log"
+
+	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/ebitenutil"
+)
+
+var (
+	bgimg *ebiten.Image
+)
+
+func main() {
+	var err error
+	bgimg, _, err = ebitenutil.NewImageFromFile("./12janggi.png", ebiten.FilterDefault)
+	if err != nil {
+		log.Fatalf("read file error: %v", err)
+	}
+
+	err = ebiten.Run(func(screen *ebiten.Image) error {
+		screen.DrawImage(bgimg, nil)
+		return nil
+	}, 500, 400, 1.0, "12 Janggi")
+
+	if err != nil {
+		log.Fatalf("Ebiten run error: %v", err)
 	}
 }
